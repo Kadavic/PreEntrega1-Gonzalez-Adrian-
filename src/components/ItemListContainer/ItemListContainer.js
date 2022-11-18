4f32c03892182ed41df7e8e1d4255bed920fe9c4
Kadavic <adriangezequiel@gmail.com>
2022-11-17 23:55:33 -0300

    General01

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ItemListContainer = ({greeting}) => {
   return (
-    <div className='text-red-700 text-2xl flex-auto'>{greeting}</div>
+    <div className='text-white-700 text-2xl flex-auto'>{greeting}</div>
   )
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,19 +3,19 @@ import CartWidget from './CartWidget'
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 bg-neutral-focus">
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-3xl">Game Shop</a>
       </div>
       <div className='flex-auto'>
-          <ul className="menu menu-horizontal p-0 ">
+          <ul className="menu menu-horizontal p-0 bg-base-300 font-bold rounded-lg">
             <li><a>Juegos</a></li>
             <li tabIndex={0}>
               <a>
-                Categoria
+                Categoría
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
               </a>
-              <ul className="p-2 bg-base-100">
+              <ul className="p-2 bg-base-100 bg-base-200 rounded-sm">
                 <li><a>Acción </a></li>
                 <li><a>Disparos</a></li>
                 <li><a>Estrategia</a></li>
